33e6eba9c63dabe16dc54f4392d8cd065fcd7512
Matias Tello <matiastello1@hotmail.com>
2022-09-27 17:08:33 -0300

    se agrego eventos

--- index.js
+++ index.js
@@ -7,20 +7,28 @@ let apellido = prompt("ingrese su apellido");
 
 saludar(nombre, apellido);
 
-let listas = document.getElementById("lista")
-const servicios = [
+alert ("Coloque el servicio que desea realizarle a su vehiculo");
+
+let servicios = [
     {id: 1, nombreServicio: "cambio de aceite", precio: 5000},
     {id: 2, nombreServicio: "cambio de filtros de aceite, combustible y habitaculo", precio: 6000},
-    {id: 3, nombreServicio: "control de los fluidos", precio: 3000}
+    {id: 3, nombreServicio: "control de los fluidos", precio: 3000},
 ];
-
-servicios.forEach(servicio => {
-    let item = document.createElement("div");
-    item.innerHTML = `
-    <h2>Id: ${servicio.id}</h2>
-    <p>Producto: ${servicio.nombreServicio}</p>
-    <p>$${servicio.precio}</p>
+  let contenedor = document.getElementById("contenedor");
+  
+  let formulario = document.getElementById("formulario");
+  formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    contenedor.innerHTML = "";
+    let inputs = e.target.children;
+  
+    let servis = servicios.find((item) => item.nombreServicio === inputs[0].value);
+    let div = document.createElement("div");
+    div.innerHTML = `
+      <h2>Id: ${servis.id}</h2>
+      <p>Nombre: ${servis.nombreServicio}</p>
+      <b>$${servis.precio}</b>
     `;
-    listas.className = "verde";
-    listas.append(item)
-});
+    div.className = "gris";
+    contenedor.append(div);
+  });
